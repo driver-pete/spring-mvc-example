e571ab36186624a30f54223aa94a14bbb4b9eea0
olegsinyavskiy <olegsinyavskiy@gmail.com>
2015-04-28 21:54:05 -0700

    hello world

---
+++ src/main/java/otognan/Application.java
@@ -0,0 +1,13 @@
+package otognan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
